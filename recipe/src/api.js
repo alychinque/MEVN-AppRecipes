a0d2fa75ae80efbdb97749b4a8f6e68f098451c7
alychinque <alychinque@gmail.com>
2022-05-17 16:05:01 +0000

    Added CRUD functions in the frontend api

---
+++ recipe/src/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const url = "/api/"
+
+export default class API{
+  static async getAllPost() {
+    const res  = await axios.get(url);
+    return res.data;
+  }
+
+  static async getRecipeByID(id) {
+    const res = await axios.get(`${url}/${id}`)
+    return res.data;
+  }
+
+  static async addRecipe(post) {
+    const res  = await axios.post(url, post);
+    return res.data;
+  }
+
+  static async updateRecipe(id, post) {
+    const res = await axios.put(`${url}/${id}`, post)
+    return res.data;
+  }
+
+  static async deleteRecipe(id) {
+    const res = await axios.delete(`${url}/${id}`)
+    return res.data;
+  }
+}
